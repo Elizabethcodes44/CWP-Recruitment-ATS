c1eeace4032980bd45c613e743c926d9f18830b2
Elizabeth <lizbeepmc@gmail.com>
2026-09-15 20:54:05 +0300

    table created for jobs with id as the primary key and auto ıncrements , title, department , location e.t.c

--- database/database.py
+++ database/database.py
@@ -10,3 +10,17 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS candidates (
 id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL, email TEXT NOT NULL UNIQUE,phone TEXT, location TEXT, skills TEXT,experience_years INTEGER)""")
 connection.commit()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS jobs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    department TEXT NOT NULL,
+    location TEXT,
+    salary_min REAL,
+    salary_max REAL,
+    status TEXT NOT NULL,
+    created_at TEXT NOT NULL
+)""")
+
+connection.commit()
